0abe8f33918f53c81ad1412c546f26c23db0bfd8
SilviaHernandezCuerdas <82943512+SilviaHernandezCuerdas@users.noreply.github.com>
2023-05-21 12:46:06 +0200

    Update H20_Analyzer.c

--- H20_Analyzer.c
+++ H20_Analyzer.c
@@ -11,7 +11,7 @@
 #define TAM_Datos 100
 
 struct Tdatos{
-	std::string parametros;
+	char parametros[PARAMETROS];
 	float ph;
 	int conductividad;
 	int turbidez;
@@ -21,21 +21,22 @@ struct Tdatos{
 struct Tfichero{
 	int anio;
 	int mes;
-	std::string lugar;
+	char lugar[PARAMETROS];
 };
 
-void media(float Tdatos datos[], int numDatos);
+float media(struct Tdatos datos[], int numDatos);
 
-void desviacion_tipica(float Tdatos datos[], int media, int numDatos);
+float desviacion_tipica(struct Tdatos datos[], float media, int numDatos);
 
 void crearFicheroDatos();
 
 int main(){
-	
-	int opcion, dato, numDatos;
-	float media, desviacion_tipica;
+	struct Tdatos datos[TAM_Datos];
+	int opcion, dato, numDatos, dato2, i;
+	float medias, desviacion_tipica;
 	
 	FILE *fichero;
+	FILE *fsalida;
 	
 	fichero = fopen("202301_Lavapies.txt", "r");
 	
@@ -43,6 +44,7 @@ int main(){
 		printf("Error, no se puede abrir el fichero.\n");
 		return 0;
 	}	
+
 	
 	printf("Bienvenido al menu de opciones de H20_ANALYZER \n");
 	printf("Escoja una de las siguientes opciones:\n");
@@ -51,48 +53,85 @@ int main(){
 	
   	switch(opcion){
                 case 1: do{
-			   printf("Seleccione los datos a tratar:\n 1)El  pH\n 2) La conductividad\n 3) La turbidez\n 4) Los coliformes\n"); 
-			   scanf("%d", &dato); 
-			}while (dato<1 || dato>4); 
+			  	printf("Seleccione los datos a tratar:\n 1) El  pH\n 2) La conductividad\n 3) La turbidez\n 4) Los coliformes\n"); 
+				scanf("%d", &dato); 
+				}while (dato<1 || dato>4); 
 						
 					switch(dato){
-						case 1: printf("Para el pH:\n");
-							media=media(float Tdatos datos[].ph, int numDatos);
-							printf("La media es: %.2f", media);
-							desviacion_tipica=desviacion_tipica(float Tdatos datos[].ph, media, int numDatos);
-							printf("La deviacion tipica es: %.2f", desviacion_tipica);
-							break;
+						case 1: do{
+							printf("Para el pH seleccione la opcion que quiera realizar:\n 1) Realizar media\n 2) Realizar desviacion tipica\n");
+							scanf("%d", &dato2);
+							}while (dato2<1 || dato2>2);
+								switch(dato2){
+									case 1: 
+										media = media(datos[numDatos].ph, numDatos);
+										printf("La media es: %.2f", media);
+									break;
+										
+									case 2:
+										desviacion_tipica = desviacion_tipica(datos[numDatos].ph, media, numDatos);
+										printf("La deviacion tipica es: %.2f", desviacion_tipica);
+									break;
+							 	}
+		
+						case 2: do{
+							printf("Para la conductividad seleccione la opcion que quiera realizar:\n 1) Realizar media\n 2) Realizar desviacion tipica\n");
+							scanf("%d", &dato2);
+							}while (dato2<1 || dato2>2);
+								switch(dato2){
+									case 1:
+										media=media(datos[TAM_Datos].conductividad, numDatos);
+										printf("La media es %.2f", media);
+									break;
+										
+									case 2:
+										desviacion_tipica = desviacion_tipica(datos[TAM_Datos].conductividad, media, numDatos);
+										printf("La deviacion tipica es: %.2f", desviacion_tipica);
+									break;	
+								}
 							
-						case 2: printf("Para la conductividad:\n");
-							media=media(float Tdatos datos[].conductividad, int numDatos);
-							printf("La media es %.2f", media);
-							desviacion_tipica=desviacion_tipica(float Tdatos datos[].conductividad, media, int numDatos);
-							printf("La deviacion tipica es: %.2f", desviacion_tipica);
-							break;	
+						case 3: do{
+							printf("Para la turbidez seleccione la opcion que quiera realizar:\n 1) Realizar media\n 2) Realizar desviacion tipica\n");
+							scanf("%d", &dato2);
+							}while (dato2<1 || dato2>2);
+								switch(dato2){
+									case 1:
+										media=media(datos[TAM_Datos].turbidez, numDatos);
+										printf("La media es: %.2f", media);
+									break;
+										
+									case 2:
+										desviacion_tipica=desviacion_tipica(datos[TAM_Datos].turbidez, media, numDatos);
+										printf("La deviacion tipica es: %.2f", desviacion_tipica);
+									break;	
+								}
 							
-						case 3: printf("Para la turbidez:\n");
-							media=media(float Tdatos datos[].turbidez, int numDatos);
-							printf("La media es: %.2f", media);
-							desviacion_tipica=desviacion_tipica(float Tdatos datos[].turbidez, media, int numDatos);
-							printf("La deviacion tipica es: %.2f", desviacion_tipica);
-							break;	
-							
-					        case 4: printf("Para los coliformes:\n");
-							media=media(float Tdatos datos[].coliformes, int numDatos);
-							printf("La media es: %.2f", media);
-							desviacion_tipica=desviacion_tipica(float Tdatos datos[].coliformes, media, int numDatos);
-							printf("La deviacion tipica es: %.2f", desviacion_tipica);
-							break;
+					        case 4: do{
+							printf("Para los coliformes seleccione la opcion que quiera realizar:\n 1) Realizar media\n 2) Realizar desviacion tipica\n");
+							scanf("%d", &dato2);
+							}while (dato2<1 || dato2>2);
+								switch(dato2){
+									case 1:
+										media=media(datos[TAM_Datos].coliformes, numDatos);
+										printf("La media es: %.2f", media);
+									break;
+											
+									case 2:
+										desviacion_tipica=desviacion_tipica(datos[TAM_Datos].coliformes, media, numDatos);
+										printf("La deviacion tipica es: %.2f", desviacion_tipica);
+									break;
+								}
 							
 					}	
 			break;
 						 
-                 case 2 : crearFicheroDatos();
-                        break;
+        	case 2 : 
+					crearFicheroDatos();
+                    break;
                        
-                 case 3 : printf("______Hasta la proxima_____\n");
-                         return 0;
-                 default : printf("Opcion desconocida\n");
+            case 3 : printf("______Hasta la proxima_____\n");
+                    return 0;
+            default : printf("Opcion desconocida\n");
     }
 
 
@@ -101,25 +140,34 @@ return 0;
 }
 
    
-void media(float Tdatos datos[], int numDatos){
-	int suma = 0, i; 
+float media(struct Tdatos datos[], int numDatos){
+	
+	float suma=0; 
 	float media = 0;
+	int i;
+	
+	
 	for(i = 0; i < numDatos; i++){
 		suma += datos[i];
 	}	
 	
-	media = suma / numDatos;
+	media = (float)(suma / numDatos);
+	
 	return media;
 }
 
-void desviacion_tipica(float Tdatos datos[], media, int numDatos){
-	int suma = 0, i;
+float desviacion_tipica(struct Tdatos datos[], float media, int numDatos){
+
+	float suma = 0;
+	int i;
 	float desviacion_tipica = 0;
+	
 	for(i = 0; i < numDatos; i++){
-		suma += pow(datos[i] - media,2);
+		suma += (float)((pow(datos[i] - media,2));
 	}	
 	
-	desviacion_tipica = pow(suma/(numDatos*(numDatos-1)) ,1/2);
+	desviacion_tipica = (float)((pow(suma/(numDatos*(numDatos-1)) ,1/2));
+	
 	return desviacion_tipica;
 }
 
@@ -169,6 +217,5 @@ void crearFicheroDatos(){
 		}
 	fclose(datosin);
 }	
-
 
 
